a961d31e6f9274287e49fdbdb19b6c0ba8ee8cfe
Twentyone-NCU <william31525@gmail.com>
2022-05-15 04:08:25 -0700

    change sh CLI_PATH

--- MultiQueue/monitor_qlens3h3s4.sh
+++ MultiQueue/monitor_qlens3h3s4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CLI_PATH=/usr/local/bin/simple_switch_CLI
+CLI_PATH=~/home/user/Desktop/oldProject/P4_Program/MultiQueue/simple_switch_CLI
 
 #get current unix time in milliseconds
 prev_time=`date +%s%N | cut -b1-13`
@@ -11,8 +11,6 @@ while true; do
   qlen2=`echo register_read qdepth 6 | $CLI_PATH --thrift-port 9092 | grep qdepth | awk '{print $3}'`
   qlen3=`echo register_read qdepth 3 | $CLI_PATH --thrift-port 9092 | grep qdepth | awk '{print $3}'`
   
-
-        
 
   now=`date +%s%N | cut -b1-13` 
   time=$(echo "scale=2; ($now -  $prev_time) / 1000.0"| bc -l)
